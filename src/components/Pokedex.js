b31e2fa5f969d50248f0d0fd200b09e36641fb98
mathpolizelli <matheuspolizelli@gmail.com>
2022-07-30 23:57:38 -0300

    finished pokedex design

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -32,11 +32,76 @@ function Pokedex() {
                 <div className="bottomLine"></div>
                 <div className="whiteFrame">
                     <div className="frameCut"></div>
+                    <div className="screenElements">
+                        <div className="topLights">
+                            <div className="smallRedLight"></div>
+                            <div className="smallRedLight"></div>
+                        </div>
+                        <div className="mainScreen"></div>
+                        <div className="bottomStuff">
+                            <div className="bigRedLight"></div>
+                            <div className="speaker"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="leftSideBottom">
+                    <div className="joystick"></div>
+                    <div className="leftSideBottomCenter">
+                        <div className="thinButtons">
+                            <div className="thinButtonRed"></div>
+                            <div className="thinButtonBlue"></div>
+                        </div>
+                        <div className="greenScreen"></div>
+                    </div>
+                    <div className="arrows">
+                        <div className="horizontal">
+                            <div className="leftArrow"></div>
+                            <div className="rightArrow"></div>
+                        </div>
+                        <div className="vertical">
+                            <div className="topArrow"></div>
+                            <div className="bottomArrow"></div>
+                        </div>
+                        <div className="center">
+                            <div className="centerCircle"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="rightSide">
                 <div className="dropLine"></div>
                 <div className="straightLine"></div>
+                <div className="rightSideElements">
+                    <div className="secondaryScreen"></div>
+                    <div className="blueButtons">
+                        <div className="blueButton"></div>
+                        <div className="blueButton"></div>
+                        <div className="blueButton"></div>
+                        <div className="blueButton"></div>
+                        <div className="blueButton"></div>
+                        <div className="blueButton"></div>
+                        <div className="blueButton"></div>
+                        <div className="blueButton"></div>
+                        <div className="blueButton"></div>
+                        <div className="blueButton"></div>
+                    </div>
+                    <div className="thinBlackButtons">
+                        <div className="thinBlackButton"></div>
+                        <div className="thinBlackButton"></div>
+                    </div>
+                    <div className="whiteButtons">
+                        <div className="leftButton"></div>
+                        <div className="rightButton"></div>
+                    </div>
+                    <div className="bigYellowButton">
+                        <div className="lighterYellow"></div>
+                        <div className="highlightYellow"></div>
+                    </div>
+                    <div className="smallScreens">
+                        <div className="smallScreen"></div>
+                        <div className="smallScreen"></div>
+                    </div>
+                </div>
             </div>
             <div className="cuts">
                 <div className="cutDrop"></div>
